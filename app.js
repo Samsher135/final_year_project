fca329087d1c8f9d4e9947b43c4fd1585cba7efc
Samsher135 <12345samsingh.samu@gmail.com>
2022-03-30 20:20:23 +0530

    range

--- app.js
+++ app.js
@@ -8,15 +8,47 @@ app.set('view engine', 'hbs')
 app.use(express.static('public')); 
 app.use(express.urlencoded({ extended: true }));
 
-let lat,lng,battery_level,range_available,device_name;
+let lat,lng,battery_level,range_available,device_name,bat_percentage;
+
+function calc_range(battery_level) {
+    let range;
+    if (battery_level >= 90 && battery_level <= 100) {
+        range = "10km";
+    }
+    else if (battery_level >= 80 && battery_level <= 89) {
+        range = "9km";
+    }
+    else if (battery_level >= 70 && battery_level <= 79) {
+        range = "8km";
+    }
+    else if (battery_level >= 60 && battery_level <= 69) {
+        range = "7km";
+    }
+    else if (battery_level >= 50 && battery_level <= 59) {
+        range = "6km";
+    }
+    else if (battery_level >= 40 && battery_level <= 49) {
+        range = "5km";
+    }
+    else if (battery_level >= 30 && battery_level <= 39) {
+        range = "4km";
+    }
+    else if (battery_level >= 20 && battery_level <= 29) {
+        range = "3km";
+    }
+    else{
+        range = "please charge the battery";
+    }
+    return range;
+}
 
 app.post('/update', (req, res)=>{
     console.log(req.body.lat);
     lat = req.body.lat;
     lng = req.body.lng;
-    battery_level = req.body.battery_level;
-    range_available = req.body.range_available;
     device_name = req.body.device;
+    battery_level = req.body.bat_percentage;
+    range_available = calc_range(parseInt(battery_level));
     res.status(200).send("Success");         
 });
 
@@ -38,7 +70,7 @@ app.post('/new_card', (req, res)=>{
     const km = d*(Math.pow(10,-3));
     console.log(km);
     if(km<100){
-        res.status(200).send({range:"inrange",lati:lat1,long:lon1,battery_level:battery_level,range_available:range_available,device_name:device_name});
+        res.status(200).send({range:"inrange",lati:lat1,long:lon1,battery_level:battery_level,range_available:range_available,device_name:device_name,bat_percentage:bat_percentage});
     }else{
         res.status(200).send({range:"outrange"});
     }
